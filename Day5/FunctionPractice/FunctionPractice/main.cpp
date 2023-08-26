e8f43049a4894c389880121a1de89ea3ff26da43
CorinneJones6 <jonescorinne6@gmail.com>
2023-08-25 19:22:16 -0600

    test

---
+++ Day5/FunctionPractice/FunctionPractice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FunctionPractice
+//
+//  Created by Corinne Jones on 8/25/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
