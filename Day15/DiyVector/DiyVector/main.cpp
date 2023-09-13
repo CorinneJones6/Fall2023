bac1ce4beb4478787988f86f76a67c3a4864bba6
CorinneJones6 <jonescorinne6@gmail.com>
2023-09-13 15:50:45 -0600

    uploading new version

--- Day15/DiyVector/DiyVector/main.cpp
+++ Day15/DiyVector/DiyVector/main.cpp
@@ -9,14 +9,6 @@
 #include "DiyVectorFuncts.hpp"
 #include "TestFuncts.hpp"
 
-
-void testDestructor () {
-    MyVector v2;
-    v2.pushBack(5);
-    v2.pushBack(20);
-    
-}
-
 int main(int argc, const char * argv[]) {
     
     TestMyVec(); 
