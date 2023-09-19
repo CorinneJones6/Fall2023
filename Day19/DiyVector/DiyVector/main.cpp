ea767be9ab02a8917aef8175a1124b79a51ba9df
CorinneJones6 <jonescorinne6@gmail.com>
2023-09-19 14:08:34 -0600

    uploading lab

---
+++ Day19/DiyVector/DiyVector/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DiyVector
+//
+//  Created by Corinne Jones on 9/12/23.
+//
+
+#include <iostream>
+#include "DiyVectorFuncts.hpp"
+#include "TestFuncts.hpp"
+
+int main(int argc, const char * argv[]) {
+    
+    TestMyVec(); 
+    TestMyAlgorithm();
+    
+    return 0;
+}
