a81d58b8b59a36089bbff5a3833a0e8a3dc5b601
CorinneJones6 <jonescorinne6@gmail.com>
2023-09-27 12:19:38 -0600

    changed to long

---
+++ CS6011/Fractions/src/Fraction.java
@@ -0,0 +1,105 @@
+//Corinne Jones
+//Fractions
+public class Fraction {
+//    public static void main(String[] args) {
+    private long numerator_, denominator_;
+
+    private long findGCD() {
+        long gcd=numerator_;
+        long remainder = denominator_;
+        while (remainder != 0) {
+            long temp = remainder;
+            remainder = gcd % remainder;
+            gcd = temp;
+        }
+        return gcd;
+    }
+
+    private void reduce () {
+        long gcd_= findGCD();
+        numerator_/= (gcd_);
+        denominator_/= (gcd_);
+    }
+    //This is a constructor when no given values.
+    public Fraction(){
+        numerator_=0;
+        denominator_=1;
+    }
+    //This is a constructor when values are given.
+    public Fraction(long n, long d){
+    long absvaln= Math.abs(n);
+    long absvald=Math.abs(d);
+        if(n<0&&d<0) {
+            numerator_ = absvaln;
+            denominator_ = absvald;
+        }
+        else if(n<0||d<0){
+            numerator_=(absvaln*(-1));
+            denominator_=absvald;
+        }
+        else{
+            numerator_=n;
+            denominator_=d;
+        }
+        reduce();
+    }
+    public Fraction plus(Fraction rhs){
+        long resultNum, resultDen;
+        if(denominator_==rhs.denominator_){
+            resultDen=denominator_;
+            resultNum=numerator_+rhs.numerator_;
+        }
+        else{
+            resultDen=denominator_*rhs.denominator_;
+            resultNum=(numerator_*rhs.denominator_)+(rhs.numerator_*denominator_);
+        }
+        return new Fraction(resultNum, resultDen);
+    }
+    public Fraction minus (Fraction rhs) {
+        long resultNum, resultDen;
+        if(denominator_==rhs.denominator_){
+            resultDen=denominator_;
+            resultNum=numerator_-rhs.numerator_;
+        }
+        else{
+            resultDen=denominator_*rhs.denominator_;
+            resultNum=(numerator_*rhs.denominator_)-(rhs.numerator_*denominator_);
+        }
+        return new Fraction(resultNum, resultDen);
+    }
+    public Fraction times (Fraction rhs){
+        long resultNum, resultDen;
+        resultDen=denominator_*rhs.denominator_;
+        resultNum=numerator_*rhs.numerator_;
+        return new Fraction(resultNum, resultDen);
+    }
+    public Fraction dividedBy (Fraction rhs){
+        long resultNum, resultDen;
+        Fraction r=new Fraction(rhs.denominator_,rhs.numerator_);
+
+        resultDen=denominator_*r.denominator_;
+        resultNum=numerator_*r.numerator_;
+
+        return new Fraction(resultNum, resultDen);
+    }
+    public Fraction reciprocal(){
+        Fraction r=new Fraction(denominator_, numerator_);
+        return r;
+    }
+
+    public String toString() {
+        String n=Long.toString(numerator_);
+        String d=Long.toString(denominator_);
+        return n+"/"+d;
+    }
+    double toDouble (){
+        double n =numerator_;
+        double d =denominator_;
+       return n/d;
+    }
+
+}
+
+
+
+
---
+++ CS6011/Fractions/src/FractionTest.java
@@ -0,0 +1,114 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class FractionTest {
+
+@Test
+public void runAllTests(){
+    testReduce();
+    testPlus();
+    testMinus();
+    testTimes();
+    testDividedBy();
+    testReciprocal();
+    testToDouble();
+
+
+}
+
+
+//This tests reduce and find GCD
+    @Test
+    void testReduce() {
+   Fraction f1=new Fraction(12,30);
+   Fraction f2=new Fraction(9,12);
+   Fraction f3=new Fraction(-6,-18);
+   Fraction f4=new Fraction(24, -108);
+
+   Assertions.assertEquals(f1.toString(), "2/5");
+   Assertions.assertEquals(f2.toString(), "3/4");
+   Assertions.assertEquals(f3.toString(), "1/3");
+   Assertions.assertEquals(f4.toString(), "-2/9");
+    }
+
+    @Test
+    void testPlus() {
+    Fraction f1=new Fraction(-3,4);
+    Fraction f2= new Fraction(1,8);
+    Fraction f3=f1.plus(f2);
+    Fraction f4= new Fraction(1, -5);
+    Fraction f5=f1.plus(f4);
+
+
+    Assertions.assertEquals(f3.toString(), "-5/8");
+    Assertions.assertEquals(f5.toString(), "-19/20");
+    }
+
+    @Test
+    void testMinus() {
+        Fraction f1=new Fraction(-3,4);
+        Fraction f2= new Fraction(1,8);
+        Fraction f3=f1.minus(f2);
+        Fraction f4= new Fraction(1, -5);
+        Fraction f5=f1.minus(f4);
+
+
+        Assertions.assertEquals(f3.toString(), "-7/8");
+        Assertions.assertEquals(f5.toString(), "-11/20");
+    }
+
+    @Test
+    void testTimes() {
+    Fraction f1= new Fraction (1,2);
+    Fraction f2= new Fraction (1, 3);
+    Fraction f3= f1.times(f2);
+
+    Assertions.assertEquals(f3.toString(), "1/6");
+    }
+
+    @Test
+    void testDividedBy() {
+        Fraction f1=new Fraction(3,5);
+        Fraction f2= new Fraction(1,-5);
+        Fraction f3=f1.dividedBy(f2);
+        Fraction f4= new Fraction(6, 7);
+        Fraction f5=f1.dividedBy(f4);
+
+
+        Assertions.assertEquals(f3.toString(), "-3/1");
+        Assertions.assertEquals(f5.toString(), "7/10");
+    }
+
+    @Test
+    void testReciprocal() {
+        Fraction f1=new Fraction(3,5);
+        Fraction f2= new Fraction(1,5);
+        Fraction f3= new Fraction(6, 7);
+
+        Assertions.assertEquals(f1.reciprocal().toString(), "5/3");
+        Assertions.assertEquals(f2.reciprocal().toString(), "5/1");
+        Assertions.assertEquals(f3.reciprocal().toString(), "7/6");
+    }
+//right now basically everything is testing this!
+    @Test
+    void testToString() {
+
+    }
+
+    @Test
+    void testToDouble() {
+        Fraction f1=new Fraction(6,3);
+        Fraction f2= new Fraction(1,5);
+        Fraction f3= new Fraction(6, 7);
+
+        double test1=f1.toDouble();
+        double test2=f2.toDouble();
+        double test3=f3.toDouble();
+
+        Assertions.assertEquals(test1, 2.0);
+        Assertions.assertEquals(test2, 0.2);
+        Assertions.assertEquals(test3, 0.8571428571428571);
+
+    }
+}
