fb7dfac6a716d2bb5a72bfed7c36b45e5895a4b6
CorinneJones6 <jonescorinne6@gmail.com>
2023-11-08 19:58:43 -0700

    finished generalizing

--- CS6012/Day2/Books/src/assignment02/LibraryTest.java
+++ CS6012/Day2/Books/src/assignment02/LibraryTest.java
@@ -11,10 +11,10 @@ class LibraryTest {
     @Test
     public void testEmpty() {
 //        Library lib = new Library();
-        LibraryGeneric<String> lib=new LibraryGeneric<>();
+        Library<String> lib=new Library<>();
         assertNull(lib.lookup(978037429279L));
 
-        ArrayList<LibraryBookGeneric<String>> booksCheckedOut = lib.lookup("Jane Doe");
+        ArrayList<LibraryBook<String>> booksCheckedOut = lib.lookup("Jane Doe");
         assertEquals(booksCheckedOut.size(), 0);
 
         assertFalse(lib.checkout(978037429279L, "Jane Doe", 1, 1, 2008));
@@ -26,7 +26,7 @@ class LibraryTest {
     public void testNonEmpty() {
 
 //        var lib = new Library();
-        LibraryGeneric<String> lib=new LibraryGeneric<>();
+        Library<String> lib=new Library<>();
         // test a small library
         lib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
         lib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
@@ -50,7 +50,7 @@ class LibraryTest {
     public void testLargeLibrary(){
         // test a medium library
 //        var lib = new Library();
-        LibraryGeneric<String>lib=new LibraryGeneric<>();
+        Library<String> lib=new Library<>();
         lib.addAll("Mushroom_Publishing.txt");
 
         //CASE: tests attempt to check out an already checked out book
@@ -74,7 +74,7 @@ class LibraryTest {
     @Test
     public void stringLibraryTest() {
         // test a library that uses names (String) to id patrons
-        LibraryGeneric<String> lib = new LibraryGeneric<>();
+        Library<String> lib = new Library<>();
         lib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
         lib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
         lib.add(9780446580342L, "David Baldacci", "Simple Genius");
@@ -100,7 +100,7 @@ class LibraryTest {
     @Test
     public void phoneNumberTest(){
         // test a library that uses phone numbers (PhoneNumber) to id patrons
-        var lib = new LibraryGeneric<PhoneNumber>();
+        var lib = new Library<PhoneNumber>();
         lib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
         lib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
         lib.add(9780446580342L, "David Baldacci", "Simple Genius");
@@ -110,7 +110,7 @@ class LibraryTest {
         assertTrue(lib.checkout(9780330351690L, patron2, 1, 1, 2008));
         assertTrue(lib.checkout(9780374292799L, patron2, 1, 1, 2008));
 
-        ArrayList<LibraryBookGeneric<PhoneNumber>> booksCheckedOut2 = lib.lookup(patron2);
+        ArrayList<LibraryBook<PhoneNumber>> booksCheckedOut2 = lib.lookup(patron2);
 
         assertEquals(booksCheckedOut2.size(), 2);
         assertTrue(booksCheckedOut2.contains(new Book(9780330351690L, "Jon Krakauer", "Into the Wild")));
@@ -122,6 +122,91 @@ class LibraryTest {
 
         assertTrue(lib.checkin(patron2));
 
+    }
+
+    @Test
+    public void overDueBookListSmallTest(){
+        // Create a library, add some books to it
+        Library<String> lib=new Library<>();
+
+
+        lib.add(1L, "Author1", "Book1");
+        lib.add(2L, "Author2", "Book2");
+        lib.add(3L, "Author3", "Book3");
+        lib.add(4L, "Author4", "Book4");
+
+        //Checkout the books with a due date
+        lib.checkout(1L, "holder1", 10, 1, 2023);
+        lib.checkout(3L, "holder3", 2, 1, 2024);
+        lib.checkout(2L, "holder2", 9, 10, 2023);
+        lib.checkout(4L, "holder4", 9, 1, 2023);
+
+        // Create an instance of OrderByDueDate
+         var overdueList=lib.getOverdueList(11, 1, 2023);
+
+        // Verify that the list is sorted in ascending order of due dates
+        assertEquals(overdueList.get(0).getIsbn(), 4);
+        assertEquals(overdueList.get(1).getIsbn(), 2);
+        assertEquals(overdueList.get(2).getIsbn(), 1);
+        assertEquals(overdueList.size(), 3);
+    }
+    @Test
+    public void overDueBookListLargeTest() {
+        Library<String> lib=new Library<>();
+        lib.addAll("Mushroom_Publishing.txt");
+
+        //Have someone check out a book
+       lib.checkout(9781843190042L, "Jane Doe", 1, 1, 2008);
+       lib.checkout(9781843190073L, "Jane Doe", 1, 1, 2008);
+       lib.checkout(9781843190110L, "Jane Doe", 1, 1, 2008);
+       lib.checkout(9781843190349L, "Jane Doe", 1, 1, 2024);
+       lib.checkout(9781843190363L, "Jane Doe", 1, 1, 2024);
+
+        var overdueList = lib.getOverdueList(11,1,2022);
+        assertEquals(overdueList.size(), 3);
+    }
+
+    @Test
+    public void orderedByAuthorTest(){
+        //initialize my library to test
+        Library<String> lib = new Library<>();
+        lib.addAll("Mushroom_Publishing.txt");
+
+        //test 1 - testing sort by author
+        var sortedByAuthor = lib.getOrderedByAuthor();
+        //System.out.println(sortedByAuthor);
+        assertEquals(sortedByAuthor.get(0).getAuthor(), "Alan Burt Akers");
+
+        //test 2 - check if the size of the sorted list matches the original library size
+        assertEquals(sortedByAuthor.size(), lib.size());
+
+        //test 3 - check that the sort function is working in ascending order
+        for (int i = 1; i < sortedByAuthor.size(); i++) {
+            String author1 = sortedByAuthor.get(i - 1).getAuthor();
+            String author2 = sortedByAuthor.get(i).getAuthor();
+            assertTrue(author1.compareTo(author2) <= 0);
+        }
+
+        //test 4 - an empty library doesn't cause an error if put through the sorted function and stays empty
+        Library<String> emptyLib = new Library<>();
+        var sortedEmptyLib = emptyLib.getOrderedByAuthor();
+        assertTrue(sortedEmptyLib.isEmpty());
+
+        //test 5 - check that an empty library throws an out of bounds error if there is an attempt to index in
+        assertThrows(IndexOutOfBoundsException.class, () -> {sortedEmptyLib.get(0);});
+
+        //test 6 - books with the same author get sorted by title
+        String sameAuthor = "Moyra Caldecott";
+        int firstIndex = sortedByAuthor.indexOf(new LibraryBook<String>(9781843190028L, sameAuthor, "Crystal Legends"));
+        int lastIndexWithSameAuthor = sortedByAuthor.lastIndexOf(new LibraryBook<String>(9781843190004L, sameAuthor, "Weapons of the Wolfhound"));
+        assertTrue(firstIndex < lastIndexWithSameAuthor);
+
+        //test 7 - check that adding a book (to the end) and then sorting puts it in the right spot
+        int origSize = lib.size();
+        lib.add(22L, "AAA New Author", "New Title");
+        assertEquals(lib.size(), origSize+1);
+        var updatedSortedByAuthor = lib.getOrderedByAuthor();
+        assertEquals(updatedSortedByAuthor.get(0).getAuthor(), "AAA New Author");
     }
 
 }
